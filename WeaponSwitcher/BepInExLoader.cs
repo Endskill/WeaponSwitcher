b3d1c14b5707085a2424574b5579e4084906423a
End skill <EndskillGaming@gmail.com>
2022-03-08 13:58:50 +0100

    Add project files.

---
+++ WeaponSwitcher/BepInExLoader.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx.IL2CPP;
+using System;
+using WeaponSwitcher.Manager;
+
+namespace WeaponSwitcher
+{
+    public class BepInExLoader : BasePlugin
+    {
+        public const string
+   MODNAME = "WeaponSwitchingApi",
+   AUTHOR = "Endskill",
+   GUID = AUTHOR + "." + MODNAME,
+   VERSION = "1.0.0";
+
+        public override void Load()
+        {
+            LogManager.SetLogger(Log);
+            LogManager._debugMessagesActive = Config.Bind("Dev Settings", "DebugMessages", false, "This settings activates/deactivates debug messages in the console for this specific plugin.").Value;
+        }
+    }
+}
---
+++ WeaponSwitcher/Model/WeaponStatus.cs
@@ -0,0 +1,20 @@
+﻿using Player;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeaponSwitcher.Model
+{
+    public class WeaponStatus
+    {
+        public WeaponStatus()
+        { }
+
+        public uint GearId { get; set; }
+
+        public AmmoType Type { get; set; }
+        public int MagazineAmmo { get; set; }
+        public int AmmoInPack { get; set; }
+
+    }
+}
